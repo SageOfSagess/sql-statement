dc12edb7451548593833964a26a9a044080468cf
schenkers <2064411@talnet.nl>
2020-09-09 14:46:23 +0200

    dit is de lost password page die bij de signup page hoort

---
+++ lostpswrd.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+
+<html>
+
+<form action="lostpswrd" method="post">
+  <input type="email"placeholder="E-mail" required/> <br>
+  <input type="submit" value="Versturen"/>
+</form>
+
+
+</html>
