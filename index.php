66db8e568c9356788c63ff9b28e2f6ee9c16b846
schenkers <2064411@talnet.nl>
2020-09-09 14:43:14 +0200

    dit is de index die hoort bij mijn signup en lostpassword page

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<html>
+<form action="index.php" method="post">
+  <input type="text" name="gnaam" placeholder="Gebruikersnaam" required /><br>
+  <input type="password" name="wachtwoord" placeholder="Wachtwoord" required /><br>
+  <input type="submit" value="Aanmelden" /><br>
+  <a href="signup.php">Nieuwe gebruiker account aanmaken</a<br>
+  <a href="lostpswrd.php"> Wachtword Vergeten</a> <br>
+
+
+
+
+
+</html>
